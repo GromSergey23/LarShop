87f4f086947b06cc6d1bdf19a0890269b8e25bfc
Grom Sergey <gromsergej13@gmail.com>
2025-06-16 13:05:49 +0200

    feat add collection_type + faker in db

---
+++ database/migrations/2025_06_16_101046_create_add_collection_product_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->enum('collection_type', ['home','work','study'])->default('home')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('add_collection_product');
+    }
+};
